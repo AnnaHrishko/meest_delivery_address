cfe244dd4c87923295a5471b736e1065b7e7ba5b
unknown <annahrishko@ukr.net>
2022-12-26 15:46:39 +0200

    meest_delivery_address правки

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-$('#copy').click(function(){
-	$('#txt').select();
-	document.execCommand('copy');
-	return false;
-})
+//$('#copy').click(function(){
+	//$('#txt').select();
+	//document.execCommand('copy');
+	//return false;
+//})
+
+//var copyTextareaBtn = document.querySelector('a.copy');
+
+$('.copy').click(function(){
+  var copyTextarea = document.querySelector('.js-copytextarea');
+  text = $(this).parent().find('p').text();
+  $('.js-copytextarea').val(text);
+  copyTextarea.focus();
+  copyTextarea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    //alert('Copying text command was ' + msg);
+  } catch (err) {
+    //console.log('Oops, unable to copy');
+  }
+  return false;
+});
+
 
 $('.country_but').click(function(){
    let index = $(this).attr("data-tag") 
